026ea17265a936828b1e3dbf91a6166cbc0bcfd8
gregoriLima <54556576+gregoriLima@users.noreply.github.com>
2020-06-18 11:04:39 -0300

    Exemplos de programação paralela do básico ao avançado em Java mostrando boas e más práticas de programação

---
+++ src/calculator/TarefaMultiplicacao.java
@@ -0,0 +1,72 @@
+package calculator;
+
+import java.math.BigInteger;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TarefaMultiplicacao implements Runnable {
+
+	private JTextField primeiro;
+   	private JTextField segundo;
+    private JLabel resultado;
+	
+    public TarefaMultiplicacao(JTextField primeiro, JTextField segundo, JLabel resultado) {
+		this.primeiro = primeiro;
+		this.segundo = segundo;
+		this.resultado = resultado;
+	}
+    
+	
+	@Override
+	public void run() { //O construtor de Thread recebe um runnable, recebe algo que roda
+		long valor1 = Long.parseLong(primeiro.getText());
+        long valor2 = Long.parseLong(segundo.getText());
+        
+        BigInteger calculo = new BigInteger("0");
+
+        for (int i = 0; i < valor1; i++) {
+            for (int j = 0; j < valor2; j++) {
+                calculo = calculo.add(new BigInteger("1"));
+            }
+        }
+
+        resultado.setText(calculo.toString());
+
+	}
+
+}
+
+
+
+//Herdar direto de thread para apenas sobreescrever o método run() não é uma boa prática
+class TarefaMultiplicacaoThread extends Thread {
+
+	private JTextField primeiro;
+   	private JTextField segundo;
+    private JLabel resultado;
+	
+    public TarefaMultiplicacaoThread(JTextField primeiro, JTextField segundo, JLabel resultado) {
+		this.primeiro = primeiro;
+		this.segundo = segundo;
+		this.resultado = resultado;
+	}
+    
+	
+	@Override
+	public void run() { //A classe thread já implementa Runnable, então pode-se herdar Thread e sobreescrever o método run()
+		long valor1 = Long.parseLong(primeiro.getText());
+        long valor2 = Long.parseLong(segundo.getText());
+        
+        BigInteger calculo = new BigInteger("0");
+
+        for (int i = 0; i < valor1; i++) {
+            for (int j = 0; j < valor2; j++) {
+                calculo = calculo.add(new BigInteger("1"));
+            }
+        }
+
+        resultado.setText(calculo.toString());
+
+	}
+}
---
+++ src/main.java
@@ -0,0 +1,18 @@
+
+public class main {
+
+	public static void main(String[] args) {
+
+			//Tudo em Java é uma thread
+		
+			System.out.println("Este proceesso é uma thread!");
+			
+			try {
+			Thread.sleep(50000);
+			} catch(Exception e) {}
+			
+			//através do jconsole executado na linha de comando é possível ver dados da thread rodando
+			
+	}
+
+}
