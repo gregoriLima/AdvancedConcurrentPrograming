4a08e4fc5f341278926883ab6a006c1db86da59f
gregoriLima <54556576+gregoriLima@users.noreply.github.com>
2020-06-19 13:48:01 -0300

    Added some wait and notify examples

---
+++ src/concorrenciaReal/ImprimeElementos.java
@@ -0,0 +1,35 @@
+package concorrenciaReal;
+
+public class ImprimeElementos implements Runnable {
+
+	private Lista lista;
+
+	public ImprimeElementos(Lista lista) {
+		// TODO Auto-generated constructor stub
+		this.lista = lista;
+	}
+
+	@Override
+	public void run() {
+
+		synchronized (lista) {
+
+			try {
+				System.out.println("Aguardando a lista ser populada!");
+				lista.wait();//Devolvendo a chave e aguardando a notificação de lista
+				//caso não seja esperado o notify de lista, o esta classe imprime os valores da lista
+				//antes de eles serem populados, sendo assim impresso vários nulls
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			for (int x = 0; x < lista.tamanho(); x++) {
+
+				System.out.println(x + " - " + lista.pegaElementos(x));
+
+			}
+		}
+	}
+
+}
